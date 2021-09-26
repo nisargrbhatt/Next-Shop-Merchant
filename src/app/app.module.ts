e9453be920b0c55d00493e95fc962d5116ecee44
nisargrbhatt <nisargrbhatt@gmail.com>
2021-09-26 21:51:17 +0530

    base module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,34 @@
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AngularMaterialModule } from './angular-material.module';
+import { SharedModule } from './shared/shared.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
+import { AuthService } from './auth/auth.service';
+import { AuthInterceptor } from './interceptor/auth.interceptor';
+import { Error404Component } from './error404/error404.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, Error404Component],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    SharedModule,
+    AngularMaterialModule,
+    HttpClientModule,
     NgbModule,
-    AngularMaterialModule
+    ReactiveFormsModule,
+    FormsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    AuthService,
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
