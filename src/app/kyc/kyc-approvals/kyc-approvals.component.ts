a6bdf8bfe39881b2f56e969cf6c81259094b7544
nisargrbhatt <nisargrbhatt@gmail.com>
2022-01-11 18:52:12 +0530

    Feature added

--- src/app/kyc/kyc-approvals/kyc-approvals.component.ts
+++ src/app/kyc/kyc-approvals/kyc-approvals.component.ts
@@ -1,25 +1,28 @@
-import {
-  FindAllKYCApprovalsResponse,
-  FindAllKYCApprovalsResponseData,
-} from './../kyc.interface';
-import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { Router } from '@angular/router';
+import { FindAllKYCApprovalsResponseData } from './../kyc.interface';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
 import { Auth0Service } from 'src/app/auth/auth0.service';
 import { KycService } from '../kyc.service';
-import { ErrorComponent } from 'src/app/shared/dialog/error/error.component';
-import { ResMesComponent } from 'src/app/shared/dialog/res-mes/res-mes.component';
-import { environment } from 'src/environments/environment';
+
 import { Auth0ProfileData } from 'src/app/auth/auth.interface';
+import { SubSink } from 'subsink';
+import { Subject, BehaviorSubject } from 'rxjs';
+import { PageEvent } from '@angular/material/paginator';
+import { distinctUntilChanged, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-kyc-approvals',
   templateUrl: './kyc-approvals.component.html',
   styleUrls: ['./kyc-approvals.component.scss'],
 })
-export class KycApprovalsComponent implements OnInit {
-  pageLoading = false;
+export class KycApprovalsComponent implements OnInit, OnDestroy {
+  private subs = new SubSink();
+
+  refresh = new Subject<any>();
+
+  totalApprovals = 0;
+  approvalsPerPage = 10;
+  currentPage = new BehaviorSubject<number>(1);
 
   kycApprovals: FindAllKYCApprovalsResponseData;
   profileClaims: Auth0ProfileData;
@@ -28,58 +31,60 @@ export class KycApprovalsComponent implements OnInit {
   constructor(
     private kycService: KycService,
     private authService: Auth0Service,
-    private snackBarService: MatSnackBar,
-    private dialogService: MatDialog,
-    private router: Router,
   ) {}
 
   ngOnInit(): void {
-    this.pageLoading = true;
     this.profileClaims = this.authService.ProfileClaims;
     this.merchantVerified = this.profileClaims.merchantVerified;
-    this.getKycApprovals();
+
+    this.subs.sink = this.kycService
+      .findAllKYCApprovals(this.currentPage.getValue(), this.approvalsPerPage)
+      .subscribe((data) => {
+        this.kycApprovals = data;
+        this.totalApprovals = data.count;
+      });
+
+    this.subs.sink = this.currentPage
+      .asObservable()
+      .pipe(
+        distinctUntilChanged(),
+        switchMap((search) =>
+          this.kycService.findAllKYCApprovals(
+            this.currentPage.getValue(),
+            this.approvalsPerPage,
+          ),
+        ),
+      )
+      .subscribe((data) => {
+        this.kycApprovals = data;
+        this.totalApprovals = data.count;
+      });
+
+    this.subs.sink = this.refresh
+      .asObservable()
+      .pipe(
+        switchMap(() =>
+          this.kycService.findAllKYCApprovals(
+            this.currentPage.getValue(),
+            this.approvalsPerPage,
+          ),
+        ),
+      )
+      .subscribe((data) => {
+        this.kycApprovals = data;
+        this.totalApprovals = data.count;
+      });
   }
 
-  async getKycApprovals(): Promise<void> {
-    this.pageLoading = true;
+  onPageChange(pageData: PageEvent): void {
+    this.currentPage.next(pageData.pageIndex + 1);
+  }
 
-    let findAllKYCApprovalsResponse: FindAllKYCApprovalsResponse;
+  onRefresh(): void {
+    this.refresh.next(2);
+  }
 
-    try {
-      findAllKYCApprovalsResponse = await this.kycService.findAllKYCApprovals();
-    } catch (error) {
-      if (error.error instanceof ErrorEvent) {
-        console.log(error);
-      } else {
-        findAllKYCApprovalsResponse = { ...error.error };
-      }
-    }
-    if (findAllKYCApprovalsResponse.valid) {
-      this.kycApprovals = findAllKYCApprovalsResponse.data;
-    } else {
-      // Open Dialog to show dialog data
-      if ('dialog' in findAllKYCApprovalsResponse) {
-        const resMesDialogRef = this.dialogService.open(ResMesComponent, {
-          data: findAllKYCApprovalsResponse.dialog,
-          autoFocus: true,
-          hasBackdrop: true,
-        });
-        await resMesDialogRef.afterClosed().toPromise();
-      }
-
-      // Open Dialog to show error data
-      if ('error' in findAllKYCApprovalsResponse) {
-        if (environment.debug) {
-          const errorDialogRef = this.dialogService.open(ErrorComponent, {
-            data: findAllKYCApprovalsResponse.error,
-            autoFocus: true,
-            hasBackdrop: true,
-          });
-          await errorDialogRef.afterClosed().toPromise();
-        }
-      }
-      this.router.navigate(['/']);
-    }
-    this.pageLoading = false;
+  ngOnDestroy(): void {
+    this.subs.unsubscribe();
   }
 }
