3d10517570c5c6f4178881617b1d1e77012259f6
nisargrbhatt <nisargrbhatt@gmail.com>
2022-03-09 19:44:03 +0530

    Lazy loading fixed

--- src/app/price/price.module.ts
+++ src/app/price/price.module.ts
@@ -4,16 +4,22 @@ import { CommonModule } from '@angular/common';
 import { PriceRoutingModule } from './price-routing.module';
 import { RouterModule } from '@angular/router';
 import { AngularMaterialModule } from './../angular-material.module';
-import { SharedModule } from './../shared/shared.module';
+// import { SharedModule } from './../shared/shared.module';
 import { PricesListComponent } from './prices-list/prices-list.component';
+import { PriceAddComponent } from '../shared/price/price-add/price-add.component';
+import { PriceShowUpdateComponent } from '../shared/price/price-show-update/price-show-update.component';
 
 @NgModule({
-  declarations: [PricesListComponent],
+  declarations: [
+    PricesListComponent,
+    PriceAddComponent,
+    PriceShowUpdateComponent,
+  ],
   imports: [
     CommonModule,
     PriceRoutingModule,
     AngularMaterialModule,
-    SharedModule,
+    // SharedModule,
     RouterModule,
   ],
 })
--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -1,27 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ProductRoutingModule } from './product-routing.module';
 import { RouterModule } from '@angular/router';
 import { AngularMaterialModule } from '../angular-material.module';
-import { SharedModule } from '../shared/shared.module';
-
+// import { SharedModule } from '../shared/shared.module';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductCategoryListComponent } from './product-category-list/product-category-list.component';
 import { ProductShowComponent } from './product-show/product-show.component';
+import { ProductCardSmallComponent } from '../shared/product/product-card-small/product-card-small.component';
+import { ProductPriceTableComponent } from '../shared/product/product-price-table/product-price-table.component';
+import { ProductScrollCardsComponent } from '../shared/product/product-scroll-cards/product-scroll-cards.component';
+import { DragScrollModule } from 'ngx-drag-scroll';
 
 @NgModule({
   declarations: [
     ProductListComponent,
     ProductCategoryListComponent,
     ProductShowComponent,
+    ProductCardSmallComponent,
+    ProductScrollCardsComponent,
+    ProductPriceTableComponent,
   ],
   imports: [
     CommonModule,
     ProductRoutingModule,
     AngularMaterialModule,
-    SharedModule,
+    // SharedModule,
     RouterModule,
+    DragScrollModule,
   ],
 })
 export class ProductModule {}
